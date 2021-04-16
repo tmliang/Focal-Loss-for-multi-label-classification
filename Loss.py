849e4cd5f6c147568a825cf575e18e4d100d4ea8
tmliang <1062075993@qq.com>
2021-04-16 12:40:35 +0800

    Create Loss.py

---
+++ Loss.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def FocalLoss(y_pred, y_true, pos_weight, gamma):
+    # y_pred is the logits without Sigmoid
+    assert y_pred.shape == y_true.shape
+    pt = torch.exp(-F.binary_cross_entropy_with_logits(y_pred, y_true, reduction='none')).detach()
+    sample_weight = (1 - pt) ** gamma
+    return F.binary_cross_entropy_with_logits(y_pred, y_true, weight=sample_weight, pos_weight=pos_weight)
